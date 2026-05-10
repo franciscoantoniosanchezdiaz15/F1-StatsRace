4c664366f53bd7467fbd866a3165496595e35c4c
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmailcom>
2026-05-10 17:27:58 +0200

    seed inicial

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,5 +5,8 @@ export FLASK_APP=app.main:app
 echo "Aplicando migraciones..."
 flask db upgrade
 
+echo "Insertando datos iniciales..."
+flask --app app.main:app seed
+
 echo "Iniciando servidor..."
 gunicorn -w 4 -b 0.0.0.0:5000 app.main:app
